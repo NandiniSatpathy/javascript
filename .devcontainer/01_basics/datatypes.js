0aa56704955d6b3832a8e1ec3b6581a3cab47009
NandiniSatpathy <36126799+NandiniSatpathy@users.noreply.github.com>
2024-02-27 07:33:01 +0000

    datatypes

---
+++ .devcontainer/01_basics/datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all JS code as newer version
+
+//alert("Hello!");  we are using nodejs not browser so this statement will give error
+
+console.log("Hello")
+
+console.log(3+3) // works fine without ;
+
+let name = "Nandini"
+let age = 34
+let isLoggedIn = true
+let state = null
+
+//number -> 2 to power 53
+//bigint
+//string ->""
+//boolean -> true/false
+//null -> standalone value, it is data type, representation of an empty value
+//undefined -> when value is not defined
+//symbol -> this is to find the uniqueness, used in scope of react, figma
+//object
+
+console.log(typeof "Hi")
+
+console.log(typeof null) //it is an object
+
+console.log(typeof undefined) // it is undefined
--- .devcontainer/01_basics/test.js
+++ .devcontainer/01_basics/test.js
@@ -1,1 +1,12 @@
-console.log("Hello World");
+const accountId = 123456;   
+let accountEmail = "satpathy.nandini@gmail.com";
+var accountPassword = "nandu";
+accountCity = "Bengaluru"; // donot use this type of declaration
+// accountId  = 2 --> not allowed const keyword cannot be changed
+console.log(accountId);
+//prefer not to use var because of issue in block scope and functional scope
+console.table([accountId, accountEmail, accountPassword, accountCity]);
+
+let x ; // x stores undefined
+
+
